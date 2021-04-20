63cefbb8cb7097e470f6285f7ac3bdb03f5f2179
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-04-20 19:14:01 -0300

    added routes in the file 🧪

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import express from 'express';
 
 const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    // res.send('Hello World, NLW#5');
+    res.json({message: "Wene Alves, Deus é Fiel"});
+});
+
+
+app.post("/post", (req, res) => {
+    const name = req.body.name;
+    const message = "Deus é fiel";
+
+    res.json({name,message});
+});
 
 app.listen(3000, () => console.log("server is running on port 3000"));
