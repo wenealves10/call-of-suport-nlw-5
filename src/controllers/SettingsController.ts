bb2a92fd2ee6ee871c40425739da6200094e55d7
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-04-23 18:50:24 -0300

    Added more Configs of Settings in the project 🥇

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,6 +1,5 @@
 import {Request, Response} from "express";
 import { SettingsService } from "../services/SettingsService";
-
 class SettingsController {
     async create(request: Request, response: Response){
     const { username, chat } = request.body;
@@ -17,6 +16,26 @@ class SettingsController {
         });
     }
    
+    }
+
+    async findByUserName(request: Request, response: Response){
+        const { username } = request.params;
+        const settingsService = new SettingsService();
+
+        const settings = await settingsService.findByUserName(username);
+
+        return response.json(settings);
+    }
+
+    async update(request: Request, response: Response){
+        const { username } = request.params;
+        const { chat } = request.body;
+
+        const settingsService = new SettingsService();
+
+        const settings = await settingsService.update(username, chat);
+
+        return response.json(settings);
     }
 }
 
