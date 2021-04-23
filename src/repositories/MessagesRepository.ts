e871723722197a9099fb977b31da6dcfdb61c679
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-04-23 18:52:03 -0300

    Added messages in the project of repository 💯

--- src/repositories/MessagesRepository.ts
+++ src/repositories/MessagesRepository.ts
@@ -1,7 +1,5 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Message } from "../entities/Message";
-
-
 @EntityRepository(Message)
 class  MessagesRepository extends Repository<Message> {}
 
