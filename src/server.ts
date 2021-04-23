a71acb5344815be02d0805df4fe6019dac7ce620
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-04-23 18:49:59 -0300

    Added More features in the project with websocket 💯

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,4 @@
-import express from 'express';
-import "./database";
-import { routes } from './routes';
+import { http } from "./http";
+import "./websocket/client";
 
-const app = express();
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(routes);
-
-
-app.listen(3000, () => console.log("server is running on port 3000"));
+http.listen(3000, () => console.log("server is running on port 3000"));
