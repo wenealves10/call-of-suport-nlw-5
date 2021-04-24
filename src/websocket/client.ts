4989c734ee1e76aea92fd141ccc8890e1d118a46
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-04-24 18:16:45 -0300

    List all the messages of Clients 🥇

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -51,5 +51,9 @@ io.on("connect", (socket) => {
             user_id,
         });
 
+        const allMessages = await messagesService.listByUser(user_id);
+
+        socket.emit("client_list_all_messages", allMessages);
+
     });
 });
